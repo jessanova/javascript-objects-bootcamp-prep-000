72aeb23d3d8da99deb465ec9ede1b715cb879688
Jess Jacobson <jessanova@gmail.com>
2018-05-15 18:28:27 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {'a':'b'}
 
 function updatePlaylist(playlist, artistName,songTitle){
-  playlist.assign(artistName,songTitle)
+  playlist.assign({artistName,songTitle})
   return playlist
 }
